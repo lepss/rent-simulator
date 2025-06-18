7a9be4fabfeabec71efb12bb1455ae2a9dbe9a04
AdrienM <a.miotton@gmail.com>
2025-06-18 19:40:33 +0200

    add accordion

--- src/components/layout/sectionLayout.tsx
+++ src/components/layout/sectionLayout.tsx
@@ -1,3 +1,9 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { AccordionItem } from "@radix-ui/react-accordion";
 import clsx from "clsx";
 import { type ElementType, type ReactNode } from "react";
 import { Separator } from "../ui/separator";
@@ -22,16 +28,24 @@ export const SectionLayout = ({
         "flex flex-col gap-4 p-4 rounded-2xl bg-white"
       )}
     >
-      <div className="flex items-center gap-4">
-        {Icon && (
-          <div className="bg-amber-400 rounded-full p-2">
-            <Icon size={32} color="white" className="" />
-          </div>
-        )}
-        <h2 className="text-2xl font-bold capitalize">{title}</h2>
-      </div>
-      <Separator className="my-4" />
-      {children}
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            <div className="flex items-center gap-4">
+              {Icon && (
+                <div className="bg-amber-400 rounded-full p-2">
+                  <Icon size={32} color="white" className="" />
+                </div>
+              )}
+              <h2 className="text-2xl font-bold capitalize">{title}</h2>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent>
+            <Separator className="my-4" />
+            {children}
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </section>
   );
 };
